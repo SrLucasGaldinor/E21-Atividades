9ff3d65bc90eea6a81428147554ff1ea71cfc4a9
Car Star SC <Lucas Galdino>
2023-09-19 15:58:53 -0300

    Arrumando erros e iniciando o CSS

--- js/header.js
+++ js/header.js
@@ -1,9 +1,11 @@
 document.write('<header class="cabecalho">');
 document.write('<a href="home.html"><img class="cabecalho-imagem" src="sua-logo.png" alt="Logomarca"></a>');
+document.write('<div class="menu-direita">')
 document.write('<nav class="cabecalho-menu">');
 document.write('<a class="cabecalho-menu-item" href="primeira.html">Atividade 1</a>');
 document.write('<a class="cabecalho-menu-item" href="segunda.html">Atividade 2</a>');
 document.write('<a class="cabecalho-menu-item" href="terceira.html">Atividade 3</a>');
 document.write('<a class="cabecalho-menu-item" href="quarta.html">Atividade 4</a>');
 document.write('</nav>');
+document.write('</div>')
 document.write('</header>');
