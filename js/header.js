d1ea812d6665a9b9d16ecac06b7724dc62c91e22
Car Star SC <Lucas Galdino>
2023-09-19 11:37:16 -0300

    Utilizando o Dom e iniciando estilização.

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.write('<header class="cabecalho">');
+document.write('<a href="index.html"><img class="cabecalho-imagem" src="sua-logo.png" alt="Logomarca"></a>');
+document.write('<nav class="cabecalho-menu">');
+document.write('<a class="cabecalho-menu-item" href="primeira.html">Atividade 1</a>');
+document.write('<a class="cabecalho-menu-item" href="segunda.html">Atividade 2</a>');
+document.write('<a class="cabecalho-menu-item" href="terceira.html">Atividade 3</a>');
+document.write('<a class="cabecalho-menu-item" href="quarta.html">Atividade 4</a>');
+document.write('</nav>');
+document.write('</header>');
