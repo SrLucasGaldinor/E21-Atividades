86e50b7e682e183b61937dbcc29e591c081a2c09
Car Star SC <Lucas Galdino>
2023-10-03 11:43:56 -0300

    Final. Media Screen que esqueci

--- html-css-js/js/header.js
+++ html-css-js/js/header.js
@@ -1,6 +1,6 @@
 document.write('<header class="cabecalho">');
 document.write('<a href="home.html"><img class="cabecalho-imagem" src="/img/logo.svg" alt="Logomarca"></a>');
-document.write('<div class="menu-direita">')
+document.write('<div>')
 document.write('<nav class="cabecalho-menu">');
 document.write('<a class="cabecalho-menu-item" href="primeira.html">Atividade 1</a>');
 document.write('<a class="cabecalho-menu-item" href="segunda.html">Atividade 2</a>');
